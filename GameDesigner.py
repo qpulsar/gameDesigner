9916b047c020037baa2c2e984950c92370686f72
ZEYNEP <zeynepyavz98@gmail.com>
2021-01-19 20:45:27 +0300

    resources>images>logo> logo1.png eklendi

--- GameDesigner.py
+++ GameDesigner.py
@@ -3,6 +3,14 @@ import sys
 import pygame
 import main
 
+"""Asagıdaki muzigin telif haklari olabilir
+sadece muzik ekleme calisiyor mu diye ekledim
+herkes bu yorum satrini okuyunca kaldirebiliriz (zy)"""
+
+music01= "./resources/musics/undertale.mp3"
+pygame.mixer.music.load(music01)
+pygame.mixer.music.play()
+
 
 if __name__ == '__main__':
     main.main()
